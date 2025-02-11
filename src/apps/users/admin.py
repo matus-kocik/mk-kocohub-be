535496b588c3e55732d67e0f78b8ada0c3562fd5
Matus <matuskocik@gmail.com>
2025-02-11 06:00:12 +0100

    update app users

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -5,14 +5,26 @@ from .models import CustomUser
 
 class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
-    list_display = ["email", "full_name", "is_active", "is_staff"]
-    search_fields = ["email", "last_name"]
-    list_filter = ["is_active", "is_staff"]
+    list_display = ["email", "full_name", "is_active", "is_staff", "date_joined"]
+    list_display_links = ["email"]
+    list_editable = [
+        "is_active",
+        "is_staff",
+    ]
+    search_fields = ["email", "first_name", "last_name"]
+    list_filter = ["is_active", "is_staff", "date_joined"]
+    readonly_fields = ["date_joined", "last_login"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal info", {"fields": ("first_name", "last_name")}),
-        ("Permissions", {"fields": ("is_active", "is_staff")}),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "groups", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("date_joined", "last_login")}),
     )
+    ordering = ["-date_joined"]
+    filter_horizontal = ["groups", "user_permissions"]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
